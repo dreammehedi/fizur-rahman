e97705f0f2d902906fc090020f6b902e32e33404
dreammehedi <dreammehedihassan@gmail.com>
2024-06-20 11:34:08 +0600

    Basic Header Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header></Header>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <h4 className="font-bold text-[18px] my-transition hover:text-cyan-400">
+      Mehedi Hassan
+    </h4>
+  );
+}
+
+export default Logo;
