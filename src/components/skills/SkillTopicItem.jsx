17fe9b6d72d7c28cd651f841fdbe3b3ac46aff3a
dreammehedi <dreammehedihassan@gmail.com>
2024-06-22 23:22:28 +0600

    Dark Mode Design Skill Topic Item

--- src/components/skills/SkillTopicItem.jsx
+++ src/components/skills/SkillTopicItem.jsx
@@ -3,7 +3,7 @@ import Tilt from "react-parallax-tilt";
 function SkillTopicItem({ skillItem }) {
   return (
     <>
-      <div className="group text-center cursor-pointer my-transition space-y-3 text-primary font-semibold rounded-xl overflow-hidden">
+      <div className="group text-center cursor-pointer my-transition space-y-3 text-primary dark:text-white font-semibold rounded-xl overflow-hidden">
         <Tilt
           perspective={600}
           glareMaxOpacity={0.55}
@@ -13,7 +13,7 @@ function SkillTopicItem({ skillItem }) {
           glarePosition="all"
           glareBorderRadius="20px"
         >
-          <div className="bg-main-bg p-6 md:p-8 rounded-xl flex flex-col justify-center items-center space-y-3 my-transition grayscale hover:grayscale-0 text-center ring-1 ring-ring-color overflow-hidden">
+          <div className="bg-main-bg dark:bg-primary/60 dark:ring-gray-800 p-6 md:p-8 rounded-xl flex flex-col justify-center items-center space-y-3 my-transition grayscale hover:grayscale-0 text-center ring-1 ring-ring-color overflow-hidden">
             <img
               src={skillItem?.icon}
               className="size-[90px] my-transition group-hover:animate-bounce group-hover:shadow-md p-2 rounded-xl object-cover overflow-hidden"
