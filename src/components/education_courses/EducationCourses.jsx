79df68be16e10975978d51bbe78ee2d039aec6ae
dreammehedi <dreammehedihassan@gmail.com>
2024-06-23 00:35:13 +0600

    Dark Mode Design Education & Courses

--- src/components/education_courses/EducationCourses.jsx
+++ src/components/education_courses/EducationCourses.jsx
@@ -1,3 +1,5 @@
+import educationData from "./EducationData";
+
 function EducationCourses() {
   return (
     <>
@@ -6,36 +8,36 @@ function EducationCourses() {
           <h2 className="bg-gradient-main-bg text-transparent bg-clip-text text-2xl md:text-3xl lg:text-4xl font-semibold text-center">
             Education & Courses
           </h2>
-          <div className="mt-6 md:mt-8 rounded-xl ring-1 ring-ring-color p-4 md:p-6 bg-main-bg grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
+          <div className="mt-6 md:mt-8 rounded-xl ring-1 ring-ring-color p-4 md:p-6 bg-main-bg dark:bg-gray-900 dark:ring-gray-800 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
             <div className="">
               <h3 className="text-2xl text-primary font-semibold mb-4">
                 Education
               </h3>
-
-              <div className="border-l border-gray-300 pl-6">
-                <div className="relative flex flex-col space-y-2">
-                  <h4 className="mydot text-black font-semibold">
-                    Higher School Certificate
-                  </h4>
-                  <h5 className="text-[18px] text-gray-800">
-                    Arfan Khan Memorial College, Lowhati
-                  </h5>
-                  <p className="text-sm">2020 - 2021</p>
-                  <span className="text-sm text-gray-500">GPA: 3.92</span>
-                </div>
-              </div>
-              <div className="pt-4 border-l border-gray-300 pl-6">
-                <div className="relative flex flex-col space-y-2">
-                  <h4 className="mydot text-black font-semibold">
-                    Secondary School Certificate
-                  </h4>
-                  <h5 className="text-[18px] text-gray-800">
-                    Fatepur M.H. High School
-                  </h5>
-                  <p className="text-sm">2018 - 2019</p>
-                  <span className="text-sm text-gray-500">GPA: 4.06</span>
-                </div>
-              </div>
+              {educationData.map((education, ind) => {
+                return (
+                  <div
+                    key={ind}
+                    className={`${
+                      ind === 1 && "pt-2"
+                    } border-l border-gray-300 pl-6`}
+                  >
+                    <div className="relative flex flex-col space-y-2">
+                      <h4 className="mydot text-black dark:text-white font-semibold">
+                        {education?.education}
+                      </h4>
+                      <h5 className="text-[18px] text-gray-800 dark:text-gray-200">
+                        {" "}
+                        {education?.schools}
+                      </h5>
+                      <p className="text-sm"> {education?.years}</p>
+                      <span className="text-sm text-gray-500 dark:text-gray-300">
+                        {" "}
+                        {education?.gpa}
+                      </span>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
             <div className="">
               <h3 className="text-2xl text-primary font-semibold mb-4">
@@ -44,10 +46,10 @@ function EducationCourses() {
 
               <div className="border-l border-gray-300 pl-6">
                 <div className="relative flex flex-col space-y-2">
-                  <h4 className="mydot text-black font-semibold">
+                  <h4 className="mydot text-black dark:text-white font-semibold">
                     Complete Web Development Course
                   </h4>
-                  <h5 className="text-[18px] text-gray-800">
+                  <h5 className="text-[18px] text-gray-800 dark:text-gray-200">
                     Programming Hero
                   </h5>
                   <p className="text-sm">January 2023 - Present</p>
---
+++ src/components/education_courses/EducationData.jsx
@@ -0,0 +1,15 @@
+const educationData = [
+  {
+    schools: "Arfan Khan Memorial College, Lowhati",
+    education: "Higher School Certificate",
+    years: "2020 - 2021",
+    gpa: "GPA: 3.92",
+  },
+  {
+    schools: "Fatepur M.H. High School",
+    education: "Secondary School Certificate",
+    years: "2018 - 2019",
+    gpa: "GPA: 4.06",
+  },
+];
+export default educationData;
