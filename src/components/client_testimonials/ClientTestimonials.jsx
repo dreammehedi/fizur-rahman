520548f0ba3be908d64bb2c4f97f674eb6b0f88a
dreammehedi <dreammehedihassan@gmail.com>
2024-06-22 16:07:24 +0600

    Client Testimonial Ui Update

--- src/components/client_testimonials/ClientTestimonials.jsx
+++ src/components/client_testimonials/ClientTestimonials.jsx
@@ -29,19 +29,19 @@ function ClientTestimonials() {
               clickable: true,
             }}
             modules={[Pagination, Autoplay]}
-            className="mt-6 md:mt-8"
+            className="mt-6 md:mt-8 p-1"
           >
             {ClientTestimonialsData?.map((testimonial, ind) => {
               return (
                 <SwiperSlide
                   key={ind}
-                  className="rounded-xl p-6 md:p-8 ring-1 ring-ring-color bg-white space-y-6 md:space-y-8"
+                  className="rounded-xl p-6 md:p-8 ring-1 ring-ring-color bg-white space-y-6 md:space-y-8 my-transition hover:ring-primary/30 hover:shadow-lg hover:shadow-primary/30 group hover:cursor-pointer"
                 >
                   <p className="font-medium">{testimonial?.feedback}</p>
                   <div className="flex items-center gap-3">
                     <a href="" target="_blank">
                       <img
-                        className="size-16 rounded-full object-cover ring-2 ring-primary/60"
+                        className="group-hover:scale-[1.08] my-transition group-hover:shadow-lg group-hover:shadow-primary/30 size-16 rounded-full object-cover ring-2 ring-primary/60 group-hover:ring-primary/30 hover:!scale-[1.5]"
                         src={testimonial?.userPhoto}
                         alt=""
                       />
